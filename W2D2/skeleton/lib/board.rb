49abbac8a5c2eeb7259f573b360f42e166516ba0
Tyler <tjb459@nyu.edu>
2019-02-12 10:07:44 -0500

    Work on mancala

---
+++ W2D2/skeleton/lib/board.rb
@@ -0,0 +1,46 @@
+class Board
+  attr_accessor :cups
+
+  def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) {Array.new}
+    place_stones 
+  end
+
+  def place_stones
+    # helper method to #initialize every non-store cup with four stones each
+    self.cups.map!.with_index do |cup, i|
+      cup += [:stone, :stone, :stone, :stone]
+    end
+    self.cups[6] = []
+    self.cups[13] = []
+  end
+
+  def valid_move?(start_pos)
+    raise "Invalid starting cup" if self.cups[start_pos].nil?
+    raise "Starting cup is empty" if self.cups[start_pos].empty?
+  end
+
+  def make_move(start_pos, current_player_name)
+    
+  end
+
+  def next_turn(ending_cup_idx)
+    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+  end
+
+  def winner
+  end
+end
